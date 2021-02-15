d2ce6944b8d3276dd705a1ad4f1a639ba724b23e
Kristy <kristy@Kristys-iMac.local>
2021-02-14 23:28:19 -0800

    0214

--- bitcoin-ticker-flutter/lib/coin_data.dart
+++ bitcoin-ticker-flutter/lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,37 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+
+class CoinData  {
+
+  double usd_rate= 0;
+  String api = '7A4F9448-EF32-40BC-8659-09A956598481';
+
+  CoinData(){
+
+  }
+
+  Future getCoinData() async{
+
+    String url = 'https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=$api';
+
+    http.Response res = await http.get(url);
+    print('print 00 -- ' + res.statusCode.toString() );
+    if(res.statusCode == 200){
+
+      //String data = res.body;
+      String data = '{
+          "time": "2021-02-13T06:39:07.9414840Z",
+    "asset_id_base": "BTC",
+    "asset_id_quote": "USD",
+    "rate": 47647.184090683273256027048103
+    }';
+      print('print 33= ' + jsonDecode(data)['rate'].toString() + " // " + res.body.toString() );
+      return jsonDecode(data)['rate'];
+    }
+
+    return 0;
+  }
+
+
+}
--- bitcoin-ticker-flutter/lib/price_screen.dart
+++ bitcoin-ticker-flutter/lib/price_screen.dart
@@ -1,11 +1,77 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform ;
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
   _PriceScreenState createState() => _PriceScreenState();
 }
 
+
+double bitcoinValueInUSD = 0;
+
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = '';
+
+  Future<void> getData() async {
+    print("start11");
+    double usd_rate = await CoinData().getCoinData();
+    setState(() {
+      bitcoinValueInUSD = usd_rate;
+    });
+
+  }
+
+
+
+  DropdownButton getDropdownButton_android() { // andorid
+    List<DropdownMenuItem> dropDownItems = [];
+    for (String each in currenciesList) {
+      dropDownItems.add(DropdownMenuItem(
+        child: Text(each),
+        value: each,
+      ));
+    }
+
+    return DropdownButton(
+      value: selectedCurrency,
+      items: dropDownItems,
+      onChanged: (newValue) {
+        setState(() {
+          selectedCurrency = newValue;
+        });
+      },
+    );
+  }
+
+  CupertinoPicker getDropdownButton_ios(){
+    List<Text> dropDownItems = [];
+    for (String each in currenciesList) {
+      dropDownItems.add(Text(each));
+    }
+
+    return CupertinoPicker(
+        backgroundColor: Colors.lightBlueAccent,
+        itemExtent: 30,
+        onSelectedItemChanged: (selectedValue) {
+          print(selectedValue);
+        },
+        children: dropDownItems
+    );
+  }
+
+
+  @override
+  void initState() {
+    print("start00");
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +93,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = '+ bitcoinValueInUSD.toString() + 'USD',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -38,14 +104,15 @@ class _PriceScreenState extends State<PriceScreen> {
             ),
           ),
           Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 30.0),
+              color: Colors.lightBlue,
+              child: Platform.isIOS ? getDropdownButton_ios() : getDropdownButton_android()
           ),
         ],
       ),
     );
   }
 }
+
